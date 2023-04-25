f692572eb1bf9a161b7aa5105f90389a74182598
bri <bcamesi@gmail.com>
2023-04-25 16:35:04 -0400

    exercise 5

--- 04-25-2023.js
+++ 04-25-2023.js
@@ -56,4 +56,14 @@ const formatDate = (date = new Date()) => {
     return `${months}/${days}/${years}`
 }
 
-console.log(formatDate())
+// console.log(formatDate())
+
+// ----------------------------------------
+
+// Exercise 5
+// Write a javascript program to create a new string adding "New!" in front of a given string. If the given string begins with "New!" already then return the original string.
+
+const addNew = (str) => 
+    str.indexOf('New!') === 0 ? str : `New! ${str}`
+
+console.log(addNew('New! Offers'))
