928ddbd638a4b078d0770b74fff8d197ab59cf8f
bri <bcamesi@gmail.com>
2023-04-25 14:29:04 -0400

    exercise 1

---
+++ 04-25-2023.js
@@ -0,0 +1,18 @@
+// April 25, 2023
+// Credit: codebubb on youtube
+
+// ----------------------------------------
+
+// Exercise 1
+// Write a Javascript program to check two numbers and return true if one of the numbers is 100 or if the sum of the two numbers is 100.
+
+const isEqualTo100 = (a,b) => a === 100 || b === 100 || (a + b) === 100;
+
+console.log(isEqualTo100(100,0))
+console.log(isEqualTo100(0,100))
+
+console.log(isEqualTo100(10,0))
+console.log(isEqualTo100(0,10))
+
+console.log(isEqualTo100(20,80))
+console.log(isEqualTo100(50,50))
