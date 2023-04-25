89af9a73dda8b2929787ddd62065cbd0878230da
bri <bcamesi@gmail.com>
2023-04-25 14:39:59 -0400

    exercise 2

--- 04-25-2023.js
+++ 04-25-2023.js
@@ -8,11 +8,21 @@
 
 const isEqualTo100 = (a,b) => a === 100 || b === 100 || (a + b) === 100;
 
-console.log(isEqualTo100(100,0))
-console.log(isEqualTo100(0,100))
+// console.log(isEqualTo100(100,0))
+// console.log(isEqualTo100(0,100))
 
-console.log(isEqualTo100(10,0))
-console.log(isEqualTo100(0,10))
+// console.log(isEqualTo100(10,0))
+// console.log(isEqualTo100(0,10))
 
-console.log(isEqualTo100(20,80))
-console.log(isEqualTo100(50,50))
+// console.log(isEqualTo100(20,80))
+// console.log(isEqualTo100(50,50))
+
+// ----------------------------------------
+
+// Exercise 2
+// Write a javascript program to get the extension of a filename.
+
+const getFileExtension = (str) => str.slice(str.lastIndexOf('.'))
+
+console.log(getFileExtension('index.html'))
+console.log(getFileExtension('webpack.config.js'))
