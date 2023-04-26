684651831343387a2adaa5b97b255b98da5f6a3b
bri <bcamesi@gmail.com>
2023-04-26 19:17:22 -0400

    2nd exercise

--- 04-26-2023.js
+++ 04-26-2023.js
@@ -14,3 +14,12 @@ const makeNewString = (str) =>
 
 // console.log(makeNewString('ab'))
 
+// ----------------------------------------
+
+// Write a javascript program to extract the first half of a string iof even length.
+
+const firstHalf = (str) => str.slice(0, str.length / 2)
+
+console.log(firstHalf('temp'))
+console.log(firstHalf('temple'))
+console.log(firstHalf('temples'))
