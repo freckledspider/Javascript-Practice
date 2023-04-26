be6ed9e48fce4a65d4efd8516a608ee3ede73414
bri <bcamesi@gmail.com>
2023-04-26 19:36:56 -0400

    exercise done

--- 04-26-2023.js
+++ 04-26-2023.js
@@ -88,3 +88,18 @@ const createArrayOfNumbers = (num) => {
 
 // console.log(countEvenNumbers2(createArrayOfNumbers(6)))
 // console.log(countEvenNumbers2(createArrayOfNumbers(9)))
+
+// ----------------------------------------
+
+// Write a javascript program to check whether a given array of integers is sorted in ascending order.
+
+const isAscending = (arr) => {
+    for (let i = 0; i < arr.length; i += 1) {
+        if (arr[i+1] < arr[i]) return false;
+    }
+    return true;
+}
+
+console.log(isAscending([1,2,3,4,5,6]))
+console.log(isAscending([1,2,5,4,3,6]))
+console.log(isAscending([1,2,3,4,6,6]))
