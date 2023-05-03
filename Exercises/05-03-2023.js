f1bb7b482a26707194bd0319e4fc7596cf70198d
bri <bcamesi@gmail.com>
2023-05-03 16:44:17 -0400

    exercise complete

--- Exercises/05-03-2023.js
+++ Exercises/05-03-2023.js
@@ -45,4 +45,21 @@ const str = `abc, def, ghi
 jkl, mno, pqr
 stu, vwx, yza`
 
-console.log(parseCSV(str))
+// console.log(parseCSV(str))
+
+// ----------------------------------------
+
+// Write a javascript program to generate a random hex color code.
+
+const getRandomHexNumber = () => 
+  Math.floor(Math.random() * 16).toString(16)
+
+const getRandomHexColor = () => 
+  '#' + Array.from({ length: 6 })
+  .map(getRandomHexNumber)
+  .join('');
+
+// console.log(getRandomHexColor())
+// console.log(getRandomHexColor())
+// console.log(getRandomHexColor())
+// console.log(getRandomHexColor())
