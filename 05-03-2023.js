e70eb1132fa2c9ca2ed0a4fa4937c330b3903349
bri <bcamesi@gmail.com>
2023-05-03 16:25:05 -0400

    exercise complete

---
+++ 05-03-2023.js
@@ -0,0 +1,15 @@
+// May 3, 2023
+// Credit: codebubb on youtube
+
+// ----------------------------------------
+
+// Given a year, report if it is a leap year.
+
+const isLeapYear = (year) => year % 4 === 0;
+
+console.log(isLeapYear(2014));
+console.log(isLeapYear(2015));
+console.log(isLeapYear(2016));
+console.log(isLeapYear(2017));
+console.log(isLeapYear(2018));
+console.log(isLeapYear(2019));
