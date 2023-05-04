61defafdba6fd4a23d5888bcf8c681860195d19a
bri <bcamesi@gmail.com>
2023-05-04 19:26:55 -0400

    exercise complete

--- Exercises/05-04-2023.js
+++ Exercises/05-04-2023.js
@@ -25,5 +25,25 @@ const countLetters = (
         .filter(s => letters.indexOf(s) > -1)
         .length;
 
-console.log(countLetters('abcde'))
-console.log(countLetters('abcde', ['b', 'c']))
+// console.log(countLetters('abcde'))
+// console.log(countLetters('abcde', ['b', 'c']))
+
+// ----------------------------------------
+
+// Write a javascript function to convert an amount to coins.  
+// Example input: 46 and possible coins 25, 10, 5, 2, 1
+// Output: 25, 10, 10, 1
+
+const countCoins = (money, coins = [25, 10, 5, 2, 1]) => {
+    const totalCoins = [];
+    for (let i=0; i<coins.length; i+=1) {
+        const thisCoinNum = Math.floor(money / coins[i]);
+        for (let y=0; y<thisCoinNum; y+=1) {
+            totalCoins.push(coins[i])
+        }
+        money -= coins[i] * thisCoinNum
+    }
+    return totalCoins
+}
+
+console.log(countCoins(46))
