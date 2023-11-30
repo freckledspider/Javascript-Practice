e5b182870250371a623c664e9085e98b816d34e8
bri <bcamesi@gmail.com>
2023-11-29 19:55:30 -0500

    some notes

---
+++ Exercises/11-29-2023.js
@@ -0,0 +1,44 @@
+// Top 25 Javascript Interview Questions by Interview Happy on YouTube
+
+// PART ONE
+// ------------------------- 1 -------------------------
+
+// What is Javascript?
+// Javascript is a programming language that is used for converting static web pages to interactive and dynamic web pages.
+
+// ------------------------- 2 -------------------------
+
+// What are data types in Javascript? What are primitive data types?
+// A data type determines the type of values that can be stored in a variable. There are two types of data types in Javascript: primitive and non-primitive.
+
+// Primitive data types: number, string, boolean, undefined, null 
+// Primitive data types are immutable, which means that their values cannot be changed or modified once they are created.
+
+// ------------------------- 3 -------------------------
+
+// What is the difference between primitive and non-primitive data types?
+
+// Non-primitive data types: object, array, function
+
+// Primitive data types can hold only single values. 
+// Non-primitive data types can hold multiple values.
+
+// Primitive data types are immutable and their values cannot be changed.
+// Non-primitive data types are mutable and their values can be changed.
+
+// Primitive data types are simple data types.
+// Non-primitive data types are complex data types.
+
+// ------------------------- 4 -------------------------
+
+// What are arrays, functions and objects?
+
+// An array is used to store a collection of values, such as a list of numbers or a list of names.
+
+// ------------------------- 5 -------------------------
+
+// To be continued in my real notebook probably.
+
+
+
+
