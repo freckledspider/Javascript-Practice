4bbd677e3a215120eec2210a4e53735b0edcfd48
bri <bcamesi@gmail.com>
2024-08-13 18:36:30 -0400

    Even or odd

---
+++ Exercises/08-13-2024.js
@@ -0,0 +1,16 @@
+// Codewars 8 kyu
+
+// Even or Odd
+// Create a function that takes an integer as an argument and returns 
+// "Even" for even numbers or "Odd" for odd numbers.
+
+// My solution:
+function evenOrOdd(number) {
+    if (number % 2 === 0) {
+      return "Even"
+    }
+    else {
+      return "Odd"
+    }
+  }
+
