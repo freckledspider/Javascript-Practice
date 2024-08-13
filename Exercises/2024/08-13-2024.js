9097da5b5b4adc06fd7632d359df2859e44053f2
bri <bcamesi@gmail.com>
2024-08-13 18:39:29 -0400

    Multiply

--- Exercises/2024/08-13-2024.js
+++ Exercises/2024/08-13-2024.js
@@ -1,5 +1,7 @@
 // Codewars 8 kyu
 
+// ----------------------------------------
+
 // Even or Odd
 // Create a function that takes an integer as an argument and returns 
 // "Even" for even numbers or "Odd" for odd numbers.
@@ -13,4 +15,17 @@ function evenOrOdd(number) {
       return "Odd"
     }
   }
+
+// ----------------------------------------
+
+// Multiply
+// This code does not execute properly. Try to figure out why.
+
+// My solution:
+function multiply(a, b){
+    return a * b
+  }
+// Was missing return
+
+// ----------------------------------------
 
