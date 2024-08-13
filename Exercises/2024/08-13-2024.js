b2d4411d8f6aca0150a729cd90bd99786c685352
bri <bcamesi@gmail.com>
2024-08-13 18:44:52 -0400

    return negative

--- Exercises/2024/08-13-2024.js
+++ Exercises/2024/08-13-2024.js
@@ -29,3 +29,17 @@ function multiply(a, b){
 
 // ----------------------------------------
 
+// Return Negative
+// In this simple assignment you are given a number and have to make it negative. 
+// But maybe the number is already negative?
+
+// My solution:
+function makeNegative(num) {
+  if (num < 0) {
+    return num
+  }
+  else {
+    return num - (num * 2);
+  }
+}
+
